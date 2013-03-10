d6bc77a8a106c6893c7cb7c67ef56438ec317412
libertyzero <liberty_zero@hotmail.com>
2013-03-10 23:02:47 +0700

    Edit spec to TDD

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,11 +2,4 @@ require 'spec_helper'
 
 describe StaticPagesController do
 
-  describe "GET 'about'" do
-    it "should be successful" do
-      get 'about'
-      response.should be_success
-    end
-  end
-
 end
--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -1,9 +1,5 @@
 require 'spec_helper'
 
 describe "Rooms" do
-  describe "GET /rooms" do
-    it "works! (now write some real specs)" do
-      get rooms_path
-    end
-  end
+  
 end
