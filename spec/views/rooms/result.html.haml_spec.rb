c5b762c4a82ee3945fcbd927b9cc45e962502051
libertyzero <liberty_zero@hotmail.com>
2013-03-10 23:05:06 +0700

    Delete some file in spec

--- spec/views/rooms/result.html.haml_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "rooms/result.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/staffs/reserve.html.haml_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "staffs/reserve.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
