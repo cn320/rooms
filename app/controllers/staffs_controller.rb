bda991a6110a19ee3da636c08214231bc87655df
libertyzero <liberty_zero@hotmail.com>
2013-03-18 07:39:15 +0700

    Add comment on Room and Staff Controller

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,6 +1,6 @@
 class StaffsController < ApplicationController
-  # GET /staffs
-  # GET /staffs.json
+  
+  # Staff index page, welcome staff
   def index
     if session[:admin] == nil
       redirect_to rooms_path
@@ -17,8 +17,7 @@ class StaffsController < ApplicationController
     end
   end
 
-  # GET /staffs/1
-  # GET /staffs/1.json
+  #show DetailRoom (1room 7day) (Staff only)
   def show
     @time = Room.all_times
     @time.push("All Free Time")
@@ -29,81 +28,40 @@ class StaffsController < ApplicationController
       @droom = DetailRoom.find(params[:id])
       @rooms = Room.find_all_by_roomname(@droom.roomname)
     end
-   # @staff = Staff.find(params[:id])
-
-    #respond_to do |format|
-     # format.html # show.html.erb
-      #format.json { render json: @staff }
-    #end
+  
   end
 
-  # GET /staffs/new
-  # GET /staffs/new.json
+ 
   def new
    
-    #@staff = Staff.new
-
-    #respond_to do |format|
-      #format.html # new.html.erb
-     # format.json { render json: @staff }
-    #end
   end
 
-  # GET /staffs/1/edit
+  
   def edit
     if session[:admin] == nil
       redirect_to rooms_path
     else   
       @room = Room.find(params[:id])
     end
-    #@staff = Staff.find(params[:id])
-  end
-
-  # POST /staffs
-  # POST /staffs.json
-  def create
- #   @staff = Staff.new(params[:staff])
-
-  #  respond_to do |format|
-   #   if @staff.save
-    #    format.html { redirect_to @staff, notice: 'Staff was successfully created.' }
-     #   format.json { render json: @staff, status: :created, location: @staff }
-     # else
-      #  format.html { render action: "new" }
-       # format.json { render json: @staff.errors, status: :unprocessable_entity }
-     # end
-    #end
-  end
-
-  # PUT /staffs/1
-  # PUT /staffs/1.json
-  def update
    
-   # @staff = Staff.find(params[:id])
-
-    #respond_to do |format|
-     # if @staff.update_attributes(params[:staff])
-       # format.html { redirect_to @staff, notice: 'Staff was successfully updated.' }
-      #  format.json { head :no_content }
-     # else
-       # format.html { render action: "edit" }
-      #  format.json { render json: @staff.errors, status: :unprocessable_entity }
-     # end
-    #end
   end
 
-  # DELETE /staffs/1
-  # DELETE /staffs/1.json
-  def destroy
-    #@staff = Staff.find(params[:id])
-    #@staff.destroy
+ 
+  def create
+ 
+  end
 
-    #respond_to do |format|
-     # format.html { redirect_to staffs_url }
-      #format.json { head :no_content }
-   # end
+ 
+  def update
+  
+  end
+
+  
+  def destroy
+   
   end
   
+  #login page
   def login
     @time = Room.all_times
     @time.push("All Free Time")
@@ -113,6 +71,7 @@ class StaffsController < ApplicationController
     end    
   end
 
+  #login and go to staff index
   def submit
     @user = params[:staff]
     @uname = Staff.find_by_username(@user["username"])
@@ -130,11 +89,13 @@ class StaffsController < ApplicationController
     end
   end
   
+  #logout
   def logout
     session[:admin] = nil
     redirect_to rooms_path
   end
 
+  #show list for DetailRoom
   def room_list
     @time = Room.all_times
     @time.push("All Free Time")
