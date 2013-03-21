e12cec98de73ed96079ce83f3255d54538db7221
libertyzero <liberty_zero@hotmail.com>
2013-03-22 04:31:23 +0700

    Add seed

--- db/migrate/20130203205151_create_staffs.rb
+++ db/migrate/20130203205151_create_staffs.rb
@@ -3,6 +3,8 @@ class CreateStaffs < ActiveRecord::Migration
     create_table :staffs do |t|
       t.string :username
       t.string :password
+      t.string :firstname
+      t.string :surname
       t.timestamps
     end
   end
